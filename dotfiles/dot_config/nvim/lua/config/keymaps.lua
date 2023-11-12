8d0d3a3680533fa43aae48353812cc3724507223
Jasper van Nieuwenhuizen <jaspervannieuwenhuizen@gmail.com>
2023-11-12 16:17:43 +0100

    neovim: Use C-M-{h,j,k,l} for navigation
    Using C-{h,j,k,l} conflicts with some other applications like ble.sh.

--- dotfiles/dot_config/nvim/lua/config/keymaps.lua
+++ dotfiles/dot_config/nvim/lua/config/keymaps.lua
@@ -2,7 +2,7 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("n", "<C-j>", "<cmd>TmuxNavigateDown<cr>", { desc = "Go to lower window", remap = false })
-vim.keymap.set("n", "<C-h>", "<cmd>TmuxNavigateLeft<cr>", { desc = "Go to left window", remap = false })
-vim.keymap.set("n", "<C-l>", "<cmd>TmuxNavigateRight<cr>", { desc = "Go to right window", remap = false })
-vim.keymap.set("n", "<C-k>", "<cmd>TmuxNavigateUp<cr>", { desc = "Go to upper window", remap = false })
+vim.keymap.set("n", "<C-M-j>", "<cmd>TmuxNavigateDown<cr>", { desc = "Go to lower window", remap = false })
+vim.keymap.set("n", "<C-M-h>", "<cmd>TmuxNavigateLeft<cr>", { desc = "Go to left window", remap = false })
+vim.keymap.set("n", "<C-M-l>", "<cmd>TmuxNavigateRight<cr>", { desc = "Go to right window", remap = false })
+vim.keymap.set("n", "<C-M-k>", "<cmd>TmuxNavigateUp<cr>", { desc = "Go to upper window", remap = false })
