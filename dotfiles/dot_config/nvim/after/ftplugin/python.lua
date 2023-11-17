935fc21cc79c5a32779bad25d62cd796de95c2d1
Jasper van Nieuwenhuizen <jaspervannieuwenhuizen@gmail.com>
2023-11-17 17:28:02 +0100

    Added config for python files
    - Added colorcolumn
    - Added autocmd to organize imports on save

---
+++ dotfiles/dot_config/nvim/after/ftplugin/python.lua
@@ -0,0 +1,18 @@
+vim.opt_local.colorcolumn = { 88 }
+
+-- Group autocmd's so they can be removed first. If we don't do this, the same
+-- autocmd is added every time we open or save a Python file.
+local pythongroup = vim.api.nvim_create_augroup("filetype_python", { clear = true })
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  pattern = "*",
+  group = pythongroup,
+  callback = function()
+    vim.lsp.buf.code_action({
+      apply = true,
+      context = {
+        only = { "source.organizeImports" },
+        diagnostics = {},
+      },
+    })
+  end,
+})
