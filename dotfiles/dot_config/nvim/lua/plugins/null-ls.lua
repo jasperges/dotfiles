f46006e19b557fb5bbbfd24773321d9d817b9c7c
Jasper van Nieuwenhuizen <jaspervannieuwenhuizen@gmail.com>
2023-12-15 14:04:51 +0100

    nvim: Removed shfmt from null-ls
    It's already being run via conform.

--- dotfiles/dot_config/nvim/lua/plugins/null-ls.lua
+++ dotfiles/dot_config/nvim/lua/plugins/null-ls.lua
@@ -2,11 +2,8 @@ return {
   "nvimtools/none-ls.nvim",
   opts = function(_, opts)
     local nls = require("null-ls")
-    opts.root_dir = opts.root_dir
-      or require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git")
     opts.sources = vim.list_extend(opts.sources or {}, {
       nls.builtins.diagnostics.mypy.with({ args = { "--python-executable", "python" } }),
-      nls.builtins.formatting.shfmt,
     })
   end,
 }
