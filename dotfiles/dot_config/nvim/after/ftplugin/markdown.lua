ee94d1002944d02f4f99083ad3779a82a5cfdb08
Jasper van Nieuwenhuizen <jaspervannieuwenhuizen@gmail.com>
2024-02-10 13:43:37 +0100

    nvim: Added markdown utility commands and shortcuts

---
+++ dotfiles/dot_config/nvim/after/ftplugin/markdown.lua
@@ -0,0 +1,84 @@
+-- Function to convert path to image link with alt text
+function _G.path_to_image_link(path)
+  -- If the path is empty, get the current line content
+  if not path then
+    path = vim.fn.getline('.')
+  end
+  for match in string.gmatch(path, "!%[%]%((.+)%)") do
+    -- We assume it's an image link without alt text
+    path = match
+    break
+  end
+  -- Prompt for the alternative text
+  local altText = vim.fn.input('Enter the title for the image: ')
+  if not altText then
+    return
+  end
+  -- Construct the markdown image syntax
+  local imageLink = string.format('![%s](%s "%s")', altText, path, altText)
+  -- Replace the current line with the image link
+  vim.api.nvim_set_current_line(imageLink)
+end
+
+-- Insert a link to a dirary file for the current date
+function _G.insert_diary_link()
+  local date = os.date('%Y-%m-%d')
+  local diaryLink = string.format('- [%s](%s)', date, date)
+  vim.api.nvim_put({ diaryLink }, 'l', true, true)
+end
+
+-- Insert the daily todo list
+function _G.insert_daily_todo()
+  local date = os.date('%Y-%m-%d')
+  local todoList = { string.format("# %s", date), "", "## To Do", "", "- [ ] " }
+  vim.api.nvim_put(todoList, 'l', false, true)
+  -- remove last empty line
+  vim.cmd('normal! Gdd')
+  -- goto insert mode and put cursor at the end of the first todo item
+  vim.cmd('startinsert!')
+end
+
+-- Helper to get the URL for a summary
+local function get_url_for_summary(summary, issues)
+  for _, issue in ipairs(issues) do
+    if issue.summary == summary then
+      return issue.url
+    end
+  end
+end
+
+-- Insert the link for an issue I am assigned to
+function _G.insert_assigned_issue_link()
+  -- Get the items from the output of a shell command
+  local issue_links = vim.fn.systemlist("get-jira-issue-links")
+  local pattern = "%[(.+)%]%((.+)%)"
+  local issues = {}
+  local issue_summaries = {}
+
+
+  for _, link in ipairs(issue_links) do
+    for summary, url in string.gmatch(link, pattern) do
+      table.insert(issues, { summary = summary, url = url })
+      table.insert(issue_summaries, summary)
+    end
+  end
+
+  vim.ui.select(
+    issue_summaries,
+    { prompt = "Select an issue" },
+    function(summary)
+      if not summary then
+        return
+      end
+      local url = get_url_for_summary(summary, issues)
+      local issueLink = string.format("- [ ] [%s](%s)", summary, url)
+      vim.api.nvim_put({ issueLink }, "l", true, true)
+    end
+  )
+end
+
+-- vim.keymap.set("n", "<leader>mi", _G.path_to_image_link, { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>mi", ":<C-u>lua _G.path_to_image_link()<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>mt", _G.insert_diary_link, { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>md", _G.insert_daily_todo, { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>ml", _G.insert_assigned_issue_link, { noremap = true, silent = true })
--- dotfiles/dot_config/nvim/lua/plugins/markdown.lua
+++ dotfiles/dot_config/nvim/lua/plugins/markdown.lua
@@ -8,7 +8,7 @@ return {
       vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
     end,
     keys = {
-      { "<leader>pp", mode = "n", "<cmd>PeekOpen<CR>", desc = "Peek Open" },
+      { "<leader>pp", mode = "n", "<cmd>PeekOpen<CR>",  desc = "Peek Open" },
       { "<leader>pc", mode = "n", "<cmd>PeekClose<CR>", desc = "Peek Close" },
     },
   },
@@ -21,8 +21,8 @@ return {
         cmp = true,
       },
       perspective = {
-        priority = "root",
-        root_tell = "index.md",
+        priority = "current",
+        root_tell = false,
         nvim_wd_heel = true,
         update = true,
       },
@@ -31,43 +31,4 @@ return {
       },
     },
   },
-  --   {
-  --     "renerocksai/telekasten.nvim",
-  --     dependencies = { "nvim-telescope/telescope.nvim", "renerocksai/calendar-vim" },
-  --     opts = {
-  --       home = vim.fn.expand("~/documents/syncthing/documenten/zettelkasten"),
-  --     },
-  --     keys = {
-  --       -- Launch panel if nothing is typed after <leader>z
-  --
-  --       { "<leader>z", mode = "n", "<cmd>Telekasten panel<CR>", desc = "Telekasten panel" },
-  --       -- Most used functions
-  --       { "<leader>zf", mode = "n", "<cmd>Telekasten find_notes<CR>", desc = "Telekasten find notes" },
-  --       { "<leader>zg", mode = "n", "<cmd>Telekasten search_notes<CR>", desc = "Telekasten search notes" },
-  --       { "<leader>zd", mode = "n", "<cmd>Telekasten goto_today<CR>", desc = "Telekasten go to today" },
-  --       { "<leader>zz", mode = "n", "<cmd>Telekasten follow_link<CR>", desc = "Telekasten follow link" },
-  --       { "<leader>zn", mode = "n", "<cmd>Telekasten new_note<CR>", desc = "Telekasten new note" },
-  --       { "<leader>zc", mode = "n", "<cmd>Telekasten show_calendar<CR>", desc = "Telekasten show calendar" },
-  --       { "<leader>zb", mode = "n", "<cmd>Telekasten show_backlinks<CR>", desc = "Telekasten show backlinks" },
-  --       { "<leader>zI", mode = "n", "<cmd>Telekasten insert_img_link<CR>", desc = "Telekasten insert image link" },
-  --
-  --       -- Call insert link automatically when we start typing a link
-  --       { "[[", mode = "i", "<cmd>Telekasten insert_link<CR>", "Telekasten insert link" },
-  --     },
-  --   },
 }
-
--- -- Prettify the whole table under the cursor:
--- :lua MdTableFormat<CR>
---
--- -- Delete the column under the cursor:
--- :lua MdTableColDelete<CR>
---
--- -- Insert a new column before the current column (where the cursor is):
--- :lua MdTableColInsert<CR>
---
--- -- Change the text alignment of the current column:
--- :lua MdTableColCircleAlign<CR>
---
--- -- Swap the current column with the one left to it:
--- :lua MdTableColSwap<CR>
