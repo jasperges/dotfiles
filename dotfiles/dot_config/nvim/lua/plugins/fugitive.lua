99ae70c14f60e7ad126f6c5442595701df2172b4
Jasper van Nieuwenhuizen <jaspervannieuwenhuizen@gmail.com>
2025-01-23 12:54:15 +0100

    fix(nvim): set <leader>gs to run fugitive Git command
    For this to work the key mapping for fzf-lua had to be disabled
    explicitly. But now it finally works again! 🎉

--- dotfiles/dot_config/nvim/lua/plugins/fugitive.lua
+++ dotfiles/dot_config/nvim/lua/plugins/fugitive.lua
@@ -1,1 +1,15 @@
-return { "tpope/vim-fugitive" }
+return {
+  {
+    "ibhagwan/fzf-lua",
+    keys = {
+      -- Explicitly disable the <leader>gs mapping
+      { "<leader>gs", false },
+    },
+  },
+  {
+    "tpope/vim-fugitive",
+    keys = {
+      { "<leader>gs", mode = "n", "<cmd>Git<cr>", desc = "Git status" },
+    },
+  },
+}
