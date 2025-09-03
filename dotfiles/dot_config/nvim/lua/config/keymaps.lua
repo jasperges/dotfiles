67d5afbcc9c448817a4bbb62021a06aacf995272
Jasper van Nieuwenhuizen <jaspervannieuwenhuizen@gmail.com>
2025-09-03 20:21:44 +0200

    fix(neovim): remove tmux navigator keymaps

--- dotfiles/dot_config/nvim/lua/config/keymaps.lua
+++ dotfiles/dot_config/nvim/lua/config/keymaps.lua
@@ -2,10 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("n", "<C-M-j>", "<cmd>TmuxNavigateDown<cr>", { desc = "Go to lower window", remap = false })
-vim.keymap.set("n", "<C-M-h>", "<cmd>TmuxNavigateLeft<cr>", { desc = "Go to left window", remap = false })
-vim.keymap.set("n", "<C-M-l>", "<cmd>TmuxNavigateRight<cr>", { desc = "Go to right window", remap = false })
-vim.keymap.set("n", "<C-M-k>", "<cmd>TmuxNavigateUp<cr>", { desc = "Go to upper window", remap = false })
 vim.keymap.set("x", "<leader>tf", ":!column -t -s '|' -o '|'<cr>", { desc = "[T]able [f]ormat", remap = false })
 vim.keymap.set("n", "<leader>]", ":<C-u>lua BlankDown(vim.v.count > 0 and vim.v.count or 1)<CR>",
   { noremap = true, silent = true })
