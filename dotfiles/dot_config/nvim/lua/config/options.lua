aede62f2fbefc34196cb541aa45eae16ca1d1600
Jasper van Nieuwenhuizen <jaspervannieuwenhuizen@gmail.com>
2025-10-04 08:07:23 +0200

    fix(nvim): disable language providers
    Probably there are no plugins I use that rely on them. And if I ever
    encounter one, I can easily enable the needed provider again.

--- dotfiles/dot_config/nvim/lua/config/options.lua
+++ dotfiles/dot_config/nvim/lua/config/options.lua
@@ -1,8 +1,12 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.g.python3_host_prog = "~/.local/pyenv/shims/python"
 vim.opt.exrc = true
 vim.opt.wrap = true
 -- Custom setting for setting the default state of Github Copilot
 vim.g.copilot_enabled = false
+-- Disable language providers
+vim.g.loaded_node_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_ruby_provider = 0
